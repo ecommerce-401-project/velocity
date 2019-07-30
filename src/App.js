9202d707bcda3f561cfbc26a7ecdde27274fe22c
Michele Saba <michelemsaba@gmail.com>
2019-07-30 15:02:28 -0500

    removed authmodal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-
 import './App.css';
 import Footer from './components/footer/';
 import Header from './components/header/';
 import Navigation from './components/navigation/';
 import Home from './components/home/';
-import AuthModal from './components/auth-modal';
 import UserLibrary from './components/userLibrary';
 
 function App() {
@@ -14,7 +12,6 @@ function App() {
     <div className="App">
       <Navigation />
       <Header />
-      <AuthModal />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/library" component={UserLibrary} />
