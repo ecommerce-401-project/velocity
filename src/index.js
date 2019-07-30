71100b752c7cf15a39ffc9221f568f263e532e6b
Michele Saba <michelemsaba@gmail.com>
2019-07-30 14:18:08 -0500

    added browser router

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import './index.css';
 import App from './App';
 import store from './redux/store';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
