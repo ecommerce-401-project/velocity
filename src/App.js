1dbc778a69198cd9bf027f3c3d6fc4e74301c2ab
Floyd Orr <floydcanhelp@gmail.com>
2019-07-30 13:39:38 -0500

    added navModal

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
     <div className="App">
       <Navigation />
       <Header />
-      <AuthModal />
       <Home />
       <Footer />
     </div>
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Col, Row } from 'reactstrap';
-import './style.scss';
+// import './style.scss';
 
 const Footer = () => {
   return (
