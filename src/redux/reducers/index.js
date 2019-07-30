220a4df4e58516420dae98072851ba8dfb821687
Michele Saba <michelemsaba@gmail.com>
2019-07-30 18:37:23 -0500

    set cookie middleware

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import currentUser from './user';
+import auth from './auth';
 import games from './games';
 
 const rootReducer = combineReducers({
   currentUser,
   games,
+  auth,
 });
 
 export default rootReducer;
