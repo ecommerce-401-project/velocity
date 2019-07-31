c12e54a2668e3ae5d0b52ee739c2dba9fab301e4
Jacob Wendt <jacob.wendt@yahoo.com>
2019-07-31 10:21:11 -0500

    Updated footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Container, Col, Row } from 'reactstrap';
+import './style.css';
+import logo from '../navigation/logo.png'
+
 const Footer = () => {
   return (
     <footer>
       <Container className="footer-body">
         <Row>
           <Col className="text-center">
-            <p className="small">VELOCITY</p>
+            <img src={logo} className='img-fluid mb-1'/>
             <hr />
           </Col>
         </Row>
-        <Row>
-          <Col>Col 1</Col>
-          <Col>Col 2</Col>
-          <Col>Col 3</Col>
+        <Row className='text-center'>
+          <Col className="media-icons"><i class="fab fa-facebook-square"></i><i class="fab fa-twitter"></i><i class="fab fa-react"></i></Col>
+          <Row>© 2019, velocity games, Inc. All rights reserved. velocity, velocity games, the velocity logo are trademarks or registered trademarks of velocity games, Inc. in the United States of America and elsewhere. Other brands or product names are the trademarks of their respective owners.  </Row>
         </Row>
       </Container>
     </footer>
